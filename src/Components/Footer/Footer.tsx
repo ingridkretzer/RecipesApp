4b73657ad1799893cbd101bc269ead568cb62abc
Antonio Lindo <antoniolvitall@gmail.com>
2024-06-10 20:40:07 -0300

    req 15

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,6 +1,17 @@
+import { Link } from 'react-router-dom';
+import drinkIcon from '../../images/drinkIcon.svg';
+import mealIcon from '../../images/mealIcon.svg';
+
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer data-testid="footer">
+      <Link to="/drinks" data-testid="drinks-bottom-btn">
+        <img src={ drinkIcon } alt="Ícone de bebida" />
+      </Link>
+      <Link to="/meals" data-testid="meals-bottom-btn">
+        <img src={ mealIcon } alt="Ícone de refeição" />
+      </Link>
+    </footer>
   );
 }
 
