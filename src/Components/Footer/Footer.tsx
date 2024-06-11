2d9b58d40f9f338461dfa9f5e77248c2173895bb
Antonio Lindo <antoniolvitall@gmail.com>
2024-06-11 00:05:39 -0300

    faltando apenas teste de cobertura

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,15 +1,33 @@
 import { Link } from 'react-router-dom';
 import drinkIcon from '../../images/drinkIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
+import styles from './Footer.module.css';
 
 function Footer() {
   return (
-    <footer data-testid="footer">
-      <Link to="/drinks" data-testid="drinks-bottom-btn">
-        <img src={ drinkIcon } alt="Ícone de bebida" />
+    <footer
+      data-testid="footer"
+      className={ styles.footer }
+    >
+      <Link
+        to="/drinks"
+      >
+        <img
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt="Ícone de bebida"
+          className={ styles.drinksBtn }
+        />
       </Link>
-      <Link to="/meals" data-testid="meals-bottom-btn">
-        <img src={ mealIcon } alt="Ícone de refeição" />
+      <Link
+        to="/meals"
+      >
+        <img
+          data-testid="meals-bottom-btn"
+          src={ mealIcon }
+          alt="Ícone de refeição"
+          className={ styles.mealsBtn }
+        />
       </Link>
     </footer>
   );
