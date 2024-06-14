11687182ec3a48f6d1af334953ae0cb827456a3b
Antonio Lindo <antoniolvitall@gmail.com>
2024-06-14 16:05:09 -0300

    testes

---
+++ src/tests/9-Profile.test.tsx
@@ -0,0 +1,33 @@
+import { screen } from '@testing-library/react'; // Import the 'screen' object from the testing library
+import AppProvider from '../Context/AppProvider';
+import { renderWithRouter } from '../Utils/renderWithRouter';
+import App from '../App';
+
+describe('Verifica as funcionalidades do Perfil', () => {
+  it('Verifica se ao ser clicado, o botao Done Recipes, redireciona para a pagina correta', async () => {
+    const { user } = renderWithRouter(<AppProvider><App /></AppProvider>, { route: '/profile' });
+
+    const profileBtn = screen.getByTestId('profile-done-btn');
+    await user.click(profileBtn);
+
+    expect(screen.getByRole('heading', { name: /Done Recipes/i })).toBeInTheDocument();
+  });
+
+  it('Verifica se ao ser clicado, o botao Favorite Recipes, redireciona para a pagina correta', async () => {
+    const { user } = renderWithRouter(<AppProvider><App /></AppProvider>, { route: '/profile' });
+
+    const profileBtn = screen.getByTestId('profile-favorite-btn');
+    await user.click(profileBtn);
+
+    expect(screen.getByRole('heading', { name: /Favorite Recipes/i })).toBeInTheDocument();
+  });
+
+  it('Verifica se ao ser clicado, o botao Logout, redireciona para a pagina correta', async () => {
+    const { user } = renderWithRouter(<AppProvider><App /></AppProvider>, { route: '/profile' });
+
+    const profileBtn = screen.getByTestId('profile-logout-btn');
+    await user.click(profileBtn);
+
+    expect(screen.getByRole('heading', { name: /Login/i })).toBeInTheDocument();
+  });
+});
