744163d5d2d3dad86b16d5abaef465f1fbefeac1
Antonio Lindo <antoniolvitall@gmail.com>
2024-06-14 15:47:17 -0300

    testando mudanca de branch

--- src/Pages/Profile/Profile.tsx
+++ src/Pages/Profile/Profile.tsx
@@ -1,18 +1,49 @@
 import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AppContext from '../../Context/AppContext';
+import profileIcon from '../../images/profileIcon.svg';
 
 function Profile() {
   const { setHeaderTitle } = useContext(AppContext);
+  const navigate = useNavigate();
 
   useEffect(() => {
     setHeaderTitle('Profile');
   }, []);
 
+  const logoutBtn = () => {
+    localStorage.clear();
+    navigate('/');
+  };
+
   return (
     <section>
+      <img src={ profileIcon } alt="Icone do Perfil" />
       <h1>
         Profile
       </h1>
+      <h2 data-testid="profile-email">email@mail.com</h2>
+
+      <button
+        data-testid="profile-done-btn"
+        onClick={ () => navigate('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <br />
+      <button
+        data-testid="profile-favorite-btn"
+        onClick={ () => navigate('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <br />
+      <button
+        data-testid="profile-logout-btn"
+        onClick={ () => logoutBtn() }
+      >
+        Logout
+      </button>
     </section>
   );
 }
